cd486cec556d29aa0958862cb87b1f53a61380eb
Julian Caicedo <me@juliancadi.com>
2018-04-10 13:01:07 +0200

    Server Side Swift with Vapor (4. Accepting Data)

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,14 +5,39 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
+  // Basic "Hello, world!" example
+  router.get("hello") { req in
+    return "Hello, world!"
+  }
+  
+  router.get("hello", "vapor") { req in
+    return "Hello Vapor"
+  }
+  
+  router.get("hello", String.parameter) { req -> String in
+    let name = try req.parameter(String.self)
+    return "Hello \(name)"
+  }
+  
+  router.post(InfoData.self, at: "info") { req, data -> String in
+    return "Hello \(data.name)"
+  }
+  
+  router.post(InfoData.self, at: "info/json") { req, data -> InfoResponse in
+    return InfoResponse(request: data)
+  }
+  
+  // Example of configuring a controller
+  let todoController = TodoController()
+  router.get("todos", use: todoController.index)
+  router.post("todos", use: todoController.create)
+  router.delete("todos", Todo.parameter, use: todoController.delete)
+}
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+struct InfoData: Content {
+  let name: String
+}
+
+struct InfoResponse: Content {
+  let request: InfoData
 }
